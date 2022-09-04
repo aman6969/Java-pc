df81960237f8366c3a7e318b48e845c03409fda0
AmanSinghRajput <88625285+aman6969@users.noreply.github.com>
2022-09-04 06:40:24 +0530

    Add files via upload

---
+++ pc_06.java
@@ -0,0 +1,15 @@
+
+import java.util.Scanner;
+public class pc_06{
+public static void main(String[]args) {
+    Scanner sc=new Scanner(System.in);
+    int age = sc.nextInt();
+    if (age>18){
+        System.out.println("you can drive");
+
+    }else
+    {
+        System.out.println("sorry you can't drive");
+    }
+}
+        }
