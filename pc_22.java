858cf8716ea164d8f497c976f2f789c1dae778ef
AmanSinghRajput <88625285+aman6969@users.noreply.github.com>
2022-09-10 06:12:18 +0530

    Add files via upload

---
+++ pc_22.java
@@ -0,0 +1,15 @@
+public class pc_22 {
+    public static void main(String[] args) {
+        int n=5;
+        int m=4;
+        for(int i=0; i<n; i++){
+            for (int j=0; j<=m; j++){
+                if(i==0
+                )
+                System.out.println("*");
+            }
+            System.out.println( );
+
+        }
+    }
+}
