dd8319e392ac52de9684334a9f36e8a1d5ac7b2f
AmanSinghRajput <88625285+aman6969@users.noreply.github.com>
2022-09-10 06:11:43 +0530

    Add files via upload

---
+++ pc_19.java
@@ -0,0 +1,11 @@
+public class pc_19 {
+    public static void main(String[] args) {
+        int i=0;
+        do{
+            System.out.println(i);
+            i=i++;
+
+        }
+        while(i<11);
+    }
+}
